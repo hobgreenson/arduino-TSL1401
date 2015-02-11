97e31f61e7380b13e05a7f2008587c5bc166e8eb
matthew green <m.hobson.green@gmail.com>
2015-02-11 17:00:28 -0600

    new full frame scripts

---
+++ line_sensor_fullframe.py
@@ -0,0 +1,36 @@
+import time
+import serial
+import numpy as np
+import matplotlib.pyplot as plt
+
+# simple script to receive data from an arduino 
+# via serial port and plot the data 
+
+port_name = "/dev/tty.usbmodem1421"
+baud = 4000000
+ser = serial.Serial(port=port_name, baudrate=baud)
+data_buffer = [] # buffer to store incoming data
+duration = 10.0 # duration (s) of reading from the serial port
+elapsed = 0 # elapsed (s) time reading from the serial port
+
+print "beginning to read data"
+ser.flushInput() # get rid of any junk in the serial buffer
+t_start = time.time()
+while (elapsed <= duration): 
+    data_buffer += ser.read(ser.inWaiting()) # blocks until all bytes read
+    elapsed = time.time() - t_start
+ser.close()
+
+data = np.array([ord(c) for c in data_buffer]) # convert buffer to an numpy array
+np.save("./fullframedata", data)
+
+#dt = elapsed / data.size # approximate sample interval
+#time = dt * np.arange(data.size) # time axis
+
+
+# plot the result
+plt.figure(1)
+plt.plot(data)
+plt.xlabel("time (s)")
+plt.ylabel("sensor output")
+plt.show()
---
+++ linearArrayCodeFullFrame.ino
@@ -0,0 +1,106 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define F_CPU 16000000UL
+#define L_ARRAY 128
+#define L_ARRAY_1 129
+
+//global vars
+uint8_t CLKcycleCounter = 0; // counts the number of clock cycles expired
+volatile uint8_t pixels[L_ARRAY] = {0}; // pixel buffer - holds data from ADC
+uint8_t i, send_pixels[L_ARRAY_1] = {0}; // i is an index for loops, m is the mimimum pixel value
+
+// ADC interrupt routine - this happens when the ADC is finished taking a sample
+ISR(ADC_vect) {
+    // if AO is sending pixel data (first 128 clock cycles of each frame)
+    // then do an ADC and put it in the pixel buffer
+    if (CLKcycleCounter < L_ARRAY) {
+        // start next CLK cycle
+        __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+        __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+        __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+        PORTB |= (1 << PINB3); // CLK = 5V
+        __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+        __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+        __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+        PORTB &= ~(1 << PINB3); // CLK = 0V
+        pixels[CLKcycleCounter] = ADCH; // high byte of ADC data
+        CLKcycleCounter++; // iterate clock cycle counter
+    } else if (CLKcycleCounter == L_ARRAY | CLKcycleCounter == L_ARRAY_1) { 
+        // there is an extra clock cycle at the end (129) with no data
+        // do a second extra clock for charge transfer
+        // | CLKcycleCounter == L_ARRAY_1
+        // start next CLK cycle
+        __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+        __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+        __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+        PORTB |= (1 << PINB3); // CLK = 5V
+        __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+        __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+        __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+        PORTB &= ~(1 << PINB3); // CLK = 0V
+        CLKcycleCounter++; // iterate clock cycle counter
+    } else { // start next frame by sending a pulse on SI
+        PORTB |= (1 << PINB0); // SI = 5V
+        __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+        __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+        PORTB |= (1 << PINB3); // CLK = 5V
+        __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+        __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+        PORTB &= ~(1 << PINB0); // SI = 0V, must go low before next clock rise!!!
+        __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+        __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+        PORTB &= ~(1 << PINB3); // CLK = 0V
+        CLKcycleCounter = 0; // reset clock cycle counter to zero  
+    }
+    ADCSRA |= (1 << ADSC); // start next conversion
+}
+
+int main(void) {
+    // use serial library for communication with MATLAB
+    Serial.begin(4000000);
+    send_pixels[L_ARRAY] = 0xFF; // end of frame flag
+  
+    // set up digital output pins on port B; CLK = B3 (pin 11) and SI = B0 (pin 8)
+    DDRB = 0; // zero the register
+    PORTB = 0; // zero the register
+    DDRB |= (1 << PINB3) | (1 << PINB0); // CLK(B3) and SI(B0)
+    PORTB &= ~(1 << PINB0); // CLK = 0V
+    PORTB &= ~(1 << PINB3); // SI = 0V
+  
+    // ADC set-up; ADC will read the analog output (AO) voltage from the sensor
+    sei(); // enable global interrupts
+    ADCSRA = 0; // zero the register
+    ADCSRA |= (1 << ADEN); // turn on ADC
+    ADCSRA |= (1 << ADPS2); // set clock to F_CPU/16 = 1Mhz
+    ADCSRA |= (1 << ADIE); // enable ADC interrupt
+    ADMUX |= (1 << ADLAR) | (1 << REFS0) | (1 << MUX2) | (1 << MUX0); // ADC is on pin 5;
+  
+    // start first frame
+    PORTB |= (1 << PINB0); // SI = 5V
+    __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+    __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+    PORTB |= (1 << PINB3); // CLK = 5V
+    __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+    __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+    PORTB &= ~(1 << PINB0); // SI = 0V, must go low before next clock rise!!!
+    __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+    __asm__("nop\n\t"); // delay one CPU clock cycle = 62.5 nanosec
+    PORTB &= ~(1 << PINB3); // CLK = 0V
+  
+    // first conversion
+    ADCSRA |= (1 << ADSC);
+
+    while (1) {
+        // copy volatile array into non-volatile array
+        for(i = 0; i < L_ARRAY; ++i) {
+            send_pixels[i] = pixels[i];
+        }
+        // send the frame
+        Serial.write(send_pixels, L_ARRAY_1); 
+    }
+}
+
+
+
+
