28476f368c22037640028368bc31894038a64bf9
Tobias Vogel <tobiasvogel@users.noreply.github.com>
2016-09-12 01:32:56 +0200

    committed symlinks by mistake, replaced by actual files

--- visualizer-app/main.cpp
+++ visualizer-app/main.cpp
@@ -1,1 +1,20 @@
-/Users/tobiasvogel/Documents/Arduino/line-sensor/line-plotter/main.cpp
+#ifndef MAIN_CPP
+#define MAIN_CPP
+
+#include <QApplication>
+#include "plotwindow.h"
+
+
+int main(int argc, char *argv[]) {
+
+	QApplication app(argc, argv);
+
+	PlotWindow plotWin;	
+
+	plotWin.show();
+
+	return app.exec();
+
+}
+
+#endif // MAIN_CPP
